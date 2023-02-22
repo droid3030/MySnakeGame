466fa369b152f8a9b5100e529788a5f056917ec8
unknown <aplute4@gmail.com>
2023-02-21 20:11:38 -0500

    GamePanel newApple method 14

--- src/GameFrame.java
+++ src/GameFrame.java
@@ -1,2 +1,16 @@
-public class GameFrame {
+import javax.swing.*;
+
+public class GameFrame extends JFrame {
+
+    GameFrame() {
+
+        this.add(new GamePanel());
+        this.setTitle("Snake");
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setResizable(false);
+        this.pack(); //fits Jframe around components set to frame
+        this.setVisible(true);
+        this.setLocationRelativeTo(null);
+
+    }
 }
--- src/SnakeGame.java
+++ src/SnakeGame.java
@@ -1,2 +1,7 @@
 public class SnakeGame {
+    public static void main(String[] args) {
+
+        new GameFrame();
+
+    }
 }
